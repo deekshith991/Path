7906c1991c60d83073cd2b808a2f70cfafd7ed69
deekshith991 <b191696@rgukt.ac.in>
2022-11-22 18:09:33 +0530

    spent some more time now working on EXERCISE 1

--- stl/Leture3/someFunctions.cpp
+++ stl/Leture3/someFunctions.cpp
@@ -25,7 +25,7 @@ int main(){
 
 	int n=29;
 	int a[n];
-	ArrInput(a,n);
+	// ArrInput(a,n);
 
 	std::vector<int> vec(n,3);
 
@@ -62,6 +62,7 @@ int main(){
 
 	// Algo for sum in range()
 
+		// [ NOTE ] can't use for big numbers
 		//  accumulate( Start_Iterator , End_Iterator , Initial_Sum );
 		int sum = accumulate( a , a+n , 0 ); // Here " 0 " is the initial value and every element in the range added to it.
 	
@@ -72,15 +73,60 @@ int main(){
 		int cnt = count( a, a+n, 3);
 
 
-	// Algo for find 1st occurence of " X "
+	// Algo for find 1st occurence of " X " 
+		// TIME COMP : O(n)
+
+	vector<int> arr= {1,2,3,4,8,9};
 
 		// find( Start_Iterator , End_Iterator , Element )
-		auto it = find( a, a+n , 8);
-			// return the Iterator of the First occurence of 8 (If it exist )
-			// else will return iterator for the positon next to last
+		auto it = find( arr.begin(), arr.end() , 8);
+		if(it == vec.end())  									// return the Iterator of the First occurence of 8 (If it exist )
+			cout << "element not found" ;     			    	// else will return iterator for the positon next to last
+		else    												// to get the position SUBTRACT BEGIN
+			cout << "found at index " << it-arr.begin(); 		// or vec.begin() work for vectors
 
-		// ______________________________________________________________________ \\
 
-		//time : 47:00
+	// binary_search
+			// it only returns TRUE or FALSE
+		bool PresentOrNot = binary_search(vec.begin(),vec.end(),8);
+
+
+	// lower_bound     [TIME COMP : log N]
+
+		// when x is passed to the function
+		// it returns the iterator to element that is not less than x
+
+		// a[n] = {1, 2, 4, 6, 8, 12, 13, 17, 17, 30}
+
+		// x=17
+		// it points to first 17 as 13 < 17 but ( 17 NOT < 17 )
+		
+		// x=25
+		// it points to 30
+		
+		// x=3424
+		// it points to the [END]
+
+	int x=13;
+	auto i = lower_bound(vec.begin(),vec.end(),x);
+
+	int position = i - vec.begin();
+
+	// upper_bound  
+
+		// it points to the element that is just GREATER than x  ;
+		
+		// a[n] = {1, 2, 4, 6, 8, 12, 13, 17, 17, 30}
+		
+		// x = 8
+		// it points to " 8 "
+
+		// x = 30 / 75756
+		// it points to "[ END ]"
+
+	auto j = upper_bound(vec.begin(),vec.end(),3);
+	int posi = j - vec.begin();
+
+	// Exercise 01 find an element x ?
 
 }
